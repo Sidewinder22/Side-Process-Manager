7674647f56750e0e21dba8ad847ac8831b2c728b
Sidewinder22 Raptor <raptor@archlinux.com>
2022-09-29 20:57:02 +0200

    Display proc directories

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,31 @@
  */
 
 #include <iostream>
+#include <filesystem>
+#include <cctype>
+#include <regex>
+
+bool isProcessDirs(const std::string& path)
+{
+	std::regex r("[0-9]");
+
+	return std::regex_search(path, r);
+}
 
 int main()
 {
 	std::cout << "### Side-Process-Manager ###" << std::endl;
+
+
+	const std::filesystem::path procDir{"/proc"};
+
+	for (auto const & entry : std::filesystem::directory_iterator{procDir})
+	{
+		if (entry.is_directory() && isProcessDirs(entry.path()))
+		{
+			std::cout << entry.path() << "\n";
+		}
+	}
+
 	return 0;
 }
